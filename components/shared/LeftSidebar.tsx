ae7ca4dd5cf57b19a236c578705086f15739afcd
NeilRow56 <neilrowland56@gmail.com>
2023-09-10 05:41:09 +0100

    Complete leftsidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,13 +1,16 @@
 'use client'
 
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 import { sidebarLinks } from '@/constants/index'
 import Image from 'next/image'
+import { SignOutButton, SignedIn } from '@clerk/nextjs'
 
 const LeftSidebar = () => {
+  const router = useRouter()
   const pathname = usePathname()
+
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
@@ -35,6 +38,22 @@ const LeftSidebar = () => {
             </Link>
           )
         })}
+      </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push('/sign-in')}>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
   )
