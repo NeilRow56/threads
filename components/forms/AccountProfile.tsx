da7991136c3f9f379808dbb780dcc9c85c42a292
NeilRow56 <neilrowland56@gmail.com>
2023-09-12 09:43:09 +0100

    Mongoose model and connection to database. User action for updating Clerk user via onboarding

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,7 +18,11 @@ import {
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
-import { UserValidation } from '@/lib/validations/users'
+
+import { useUploadThing } from '@/lib/uploadthing'
+import { isBase64Image } from '@/lib/utils'
+import { UserValidation } from '@/lib/validations/user'
+import { updateUser } from '@/lib/actions/user.actions'
 
 interface Props {
   user: {
@@ -35,6 +39,7 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const router = useRouter()
   const pathname = usePathname()
+  const { startUpload } = useUploadThing('media')
 
   const [files, setFiles] = useState<File[]>([])
 
@@ -51,14 +56,23 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
   const onSubmit = async (values: z.infer<typeof UserValidation>) => {
     const blob = values.profile_photo
 
-    // await updateUser({
-    //   name: values.name,
-    //   path: pathname,
-    //   username: values.username,
-    //   userId: user.id,
-    //   bio: values.bio,
-    //   image: values.profile_photo,
-    // });
+    const hasImageChanged = isBase64Image(blob)
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files)
+
+      if (imgRes && imgRes[0].url) {
+        values.profile_photo = imgRes[0].url
+      }
+    }
+
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname,
+    })
 
     if (pathname === '/profile/edit') {
       router.back()
