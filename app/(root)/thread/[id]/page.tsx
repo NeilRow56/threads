e13edf1c498017706aa9b22c6bab3c4c9ca5e281
NeilRow56 <neilrowland56@gmail.com>
2023-09-13 16:44:53 +0100

    Completed individual thread page with comments

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -4,6 +4,7 @@ import { currentUser } from '@clerk/nextjs'
 import ThreadCard from '@/components/cards/ThreadCard'
 import { fetchUser } from '@/lib/actions/user.actions'
 import { fetchThreadById } from '@/lib/actions/thread.actions'
+import Comment from '@/components/forms/Comment'
 
 export const revalidate = 0
 
@@ -32,6 +33,31 @@ const DetailedThreadPage = async ({ params }: { params: { id: string } }) => {
           createdAt={thread.createdAt}
           comments={thread.children}
         />
+      </div>
+
+      <div className="mt-7">
+        <Comment
+          threadId={thread.id}
+          currentUserImg={userInfo.image}
+          currentUserId={JSON.stringify(userInfo._id)}
+        />
+      </div>
+
+      <div className="mt-10">
+        {thread.children.map((childItem: any) => (
+          <ThreadCard
+            key={childItem._id}
+            id={childItem._id}
+            currentUserId={user.id}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
       </div>
     </section>
   )
